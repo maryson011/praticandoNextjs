2853ee24d91643687aa74d04bfde910098701c04
maryson gleison <maryson011@gmail.com>
2024-07-11 10:59:29 -0300

    refactor - utilizando o children para receber o texto do componete 'MenuItem'

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function MenuItem(props: any) {
     return (
         <Link href={props.link} className="hover:bg-black p-2">
-            {props.texto}
+            {props.children}
         </Link>
     )
 }
--- src/components/template/MenuPrincipal.tsx
+++ src/components/template/MenuPrincipal.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+
 import MenuItem from "./MenuItem";
 
 export default function MenuPrincipal() {
@@ -6,9 +6,9 @@ export default function MenuPrincipal() {
     return (
         <aside className="w-60 bg-zinc-900">
             <nav className="flex flex-col p-5 gap-1">
-                <MenuItem texto='Primeiro Componente' link='/primeiro' />
-                <MenuItem texto='Flexbox' link='/flexbox' />
-                <MenuItem texto='Componente Página' link='/pagina' />
+                <MenuItem link='/primeiro'>Primeiro Componente</MenuItem>
+                <MenuItem link='/flexbox'>Flexbox</MenuItem>
+                <MenuItem link='/pagina'>Componente Página</MenuItem>
                 {/* {menuItem('/primeiro', 'Primeiro Componente')}
                 {menuItem('/flexbox', 'Flexbox')}
                 {menuItem('/pagina', 'Componente Página')} */}
