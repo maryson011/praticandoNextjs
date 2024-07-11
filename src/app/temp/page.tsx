2f288d3d5fad731012621dda30b049dcabeeb2e8
maryson gleison <maryson011@gmail.com>
2024-07-10 23:00:59 -0300

    feat - links de redirecionamento

---
+++ src/app/temp/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <>
+            <h1>Page</h1>
+            <nav>
+                <Link href="/">Home</Link>
+            </nav>
+        </>
+    )
+}
